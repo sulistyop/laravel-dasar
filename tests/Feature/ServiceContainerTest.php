9663501481110ae5c11f5b03fd73365d9a82a4a4
sulistyp <sulisdeveloper@gmail.,com>
2023-07-23 14:56:50 +0700

    Dependency injection with bind

--- tests/Feature/ServiceContainerTest.php
+++ tests/Feature/ServiceContainerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Data\Foo;
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -18,4 +19,20 @@ class ServiceContainerTest extends TestCase
         self::assertEquals("Foo", $foo2->foo());
         self::assertNotSame($foo1, $foo2);
     }
+    public function testBind()
+    {
+        // Closure
+        $this->app->bind(Person::class, function ($app){
+            return new Person("Sulistyo","Pradana");
+        });
+
+        $person1 = $this->app->make(Person::class);// closure() // new Person("Sulistyo","Pradana");
+        $person2 = $this->app->make(Person::class);// closure() // new Person("Sulistyo","Pradana");
+
+        self::assertEquals('Sulistyo', $person1->firstName);
+        self::assertEquals('Sulistyo', $person2->firstName);
+
+        self::assertNotSame($person1, $person2);
+    }
+
 }
